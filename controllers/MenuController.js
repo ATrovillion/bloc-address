d1203b1f384b212f8127052a4b3718f519efba0b
Aaron Trovillion <aaron.trovillion@gmail.com>
2018-12-30 08:22:19 -0500

    add prompt and menu assignment work

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -9,7 +9,8 @@ module.exports = class MenuController {
                     message: "Please choose from an option below: ",
                     choices: [
                         "Add new contact",
-                        "Exit"
+                        "Exit",
+                        "Check date"
                     ]
             }
         ];
@@ -25,6 +26,9 @@ module.exports = class MenuController {
                     break;
                 case "Exit":
                     this.exit();
+                case "Check date":
+                    this.getDate();
+                    break;
                 default:
                     console.log("Invalid input");
                     this.main();
@@ -49,5 +53,12 @@ module.exports = class MenuController {
     exit() {
         console.log("Thanks for using AddressBloc!");
         process.exit();
+    }
+
+    getDate() {
+        var datetime = new Date();
+        this.clear();
+        console.log("Today's date is " + datetime);
+        this.main();
     }
 }

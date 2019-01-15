9e338f6ce2153fb3cfbc6129a51708a1182ee93d
Aaron Trovillion <aaron.trovillion@gmail.com>
2019-01-15 15:39:33 -0500

    add completed dbconnect checkpoint work

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -14,6 +14,22 @@ describe("ContactController", () => {
         });
     });
 
+    describe("#addContact()", () => {
+        it("should add a single contact into the book", (done) => {
+
+            this.book.addContact("Alice", "001-101-1010")
+            .then((contact) => {
+
+                expect(contact.name).toBe("Alice");
+                expect(contact.phone).toBe("001-101-1010");
+                done();
+            })
+            .catch((err) => {
+                done();
+            });
+        });
+    })
+
     it("should be defined", () => {
         expect(ContactController).toBeDefined();
     });
--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -6,19 +6,6 @@ describe("MenuController", () => {
         this.menu = new MenuController();
     });
 
-    describe("#getContactCount()", () => {
-
-        it("should return 0 when no contacts are in the book", () => {
-            const menu = new MenuController();
-            expect(menu.getContactCount()).toBe(0);
-        });
-
-        it("should return 1 when there is exactly one contact in the book", () => {
-            this.menu.book.addContact("Bob", "555-555-5555");
-            expect(this.menu.getContactCount()).toBe(1)
-        });
-    });
-
     describe("#remindMe()", () => {
 
         it("should return a string containing the text 'Learning is a life-long pursuit'", () => {
